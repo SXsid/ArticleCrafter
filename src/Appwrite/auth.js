1ff96bfe65dd49a3730791619e099881212026d1
sudhanshu20204 <sidshekhar2024@gmail.com>
2024-11-12 08:30:17 +0530

    auth setup

---
+++ src/Appwrite/auth.js
@@ -0,0 +1,49 @@
+import {Client,Account, ID} from "appwrite"
+import Config from "../config/config"
+//we need to use class to we can exprot them as a custom mehtodn rather than dirclty expositn the appwritre funtion
+
+export class AuthService{
+    client=new Client()
+    account;
+    // i wnat both to intialize when an objxt is created 
+    constructor(){
+        this.client.setEndpoint(Config.appwriteUrl).setProject(Config.appWriteProjectId)
+        this.account= new Account(this.client)
+
+    }
+    //cusotm fucntion which wil use appwrite under the hood
+    async CreateAccount({name,email,password}){
+        try{
+            const UserAccount= await this.account.create(ID.unique(),name,email,password)
+            if(!UserAccount){
+                throw new Error
+            }else{
+                this.Login(email,password)
+            }
+        }catch(e){
+            throw e
+        }
+    }
+
+    async Login({email,password}){
+        try{
+            const logValue= await this.account.createEmailPasswordSession(email,password)
+            return logValue
+        }catch(e){
+            throw e
+        }
+    }
+    async LogOut(){
+        try{
+            await this.account.deleteSessions()
+        }catch(e){
+            throw e
+        }
+    }
+    async GetUser(){
+        
+        const User= await this.account.get()
+        return User
+    }
+}
+export const authService = new AuthService()
